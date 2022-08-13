cb8e44782bff76489d07dedbd2dd63949f5a2eb6
garlic0x1 <garlic1@protonmail.com>
2022-08-12 22:14:27 -0700

    xor decrypt

--- src/crypto.rs
+++ src/crypto.rs
@@ -1,15 +1,7 @@
 /// decrypt ascii text (as used in PE XOR problem)
 pub fn xor_decimal_text(text: &Vec<u32>, key: &Vec<u32>) -> Vec<u32> {
-    let mut vec = Vec::new();
-
-    let mut i = 0;
-    for dec in text {
-        let k = key[i % key.len()];
-
-        vec.push(*dec ^ k);
-
-        i += 1;
-    }
-
-    vec
+    text.iter()
+        .enumerate()
+        .map(|(i, n)| n ^ key[i % key.len()])
+        .collect()
 }
